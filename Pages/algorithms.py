ff3a2eaa78252585469a488dc8604dbde7846ce1
itachikryst <44116634+itachikryst@users.noreply.github.com>
2021-04-24 17:18:30 +0200

    minor fix

--- Pages/algorithms.py
+++ Pages/algorithms.py
@@ -152,20 +152,36 @@ def attribute_weights_on_test_sample_algorithm(user_model_instance, algorithm):
 def exponential_attribute_weights_on_test_sample_algorithm(user_model_instance,
                                                            algorithm):
     selections = SelectionModel.objects.filter(user__exact=user_model_instance)
-    photos_ranked = [0] * 40
+    # photos_ranked = [0] * 40
+    plusone = [[0, 0]] * 40
+    minusone = [[0, 0]] * 40
     for selection in selections:
-        if selection.selection:
-            photos_ranked = [x + y for x, y in zip(photos_ranked,
-                                                   selection.photo.get_attributes())]
-        else:
-            photos_ranked = [x - y for x, y in zip(photos_ranked,
-                                                   selection.photo.get_attributes())]
+        attributes = selection.photo.get_attributes()
+        isliked = selection.selection
+        for x in range(40):
+            if attributes[x] == 1:
+                plusone[x][1] += 1
+                if isliked:
+                    plusone[x][0] += 1
+            elif attributes[x] == -1:
+                minusone[x][1] += 1
+                if isliked:
+                    minusone[x][0] += 1
     photos = PhotoModel.objects.all()
     best_list = []
     biggest_score = -1000000
     for photo in photos:
-        score = sum(
-            [x * y for x, y in zip(photos_ranked, photo.get_attributes())])
+        score = 0
+        attributes = photo.get_attributes()
+        for x in range(40):
+            if attributes[x] == 1:
+                score += attributes[x] * (
+                            plusone[x][0] * plusone[x][0] * plusone[x][0] /
+                            plusone[x][1])
+            elif attributes[x] == -1:
+                score += attributes[x] * (
+                            minusone[x][0] * minusone[x][0] * minusone[x][0] /
+                            minusone[x][1])
         if score > biggest_score:
             biggest_score = score
             best_list = [photo]
@@ -173,3 +189,4 @@ def exponential_attribute_weights_on_test_sample_algorithm(user_model_instance,
             best_list.append(photo)
     algorithm.photo = choice(best_list)
     algorithm.save()
+
