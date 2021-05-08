14357c23c021ab15ac8df2d140ea4697fef10625
itachikryst <44116634+itachikryst@users.noreply.github.com>
2021-05-08 18:37:36 +0200

    code rework

--- Pages/algorithms.py
+++ Pages/algorithms.py
@@ -1,6 +1,8 @@
 from random import choice
 
-from Models.models import AlgorithmModel, PhotoModel, UserModel, SelectionModel
+from Models.models import PhotoModel, SelectionModel
+
+PHOTOS = PhotoModel.objects.all()
 
 
 def linear_adding_algorithm(user_model_instance, algorithm):
@@ -13,10 +15,9 @@ def linear_adding_algorithm(user_model_instance, algorithm):
         else:
             photos_ranked = [x - y for x, y in zip(photos_ranked,
                                                    selection.photo.get_attributes())]
-    photos = PhotoModel.objects.all()
     best_list = []
     biggest_score = -1000000
-    for photo in photos:
+    for photo in PHOTOS:
         score = sum(
             [x * y for x, y in zip(photos_ranked, photo.get_attributes())])
         if score > biggest_score:
@@ -38,10 +39,9 @@ def exponential_adding_algorithm(user_model_instance, algorithm):
         else:
             photos_ranked = [x - y for x, y in zip(photos_ranked,
                                                    selection.photo.get_attributes())]
-    photos = PhotoModel.objects.all()
     best_list = []
     biggest_score = -100000000000
-    for photo in photos:
+    for photo in PHOTOS:
         score = sum(
             [x * x * x * y for x, y in
              zip(photos_ranked, photo.get_attributes())])
@@ -64,8 +64,6 @@ def selection_average_algorithm(user_model_instance, algorithm):
         else:
             photos_ranked = [x - y for x, y in zip(photos_ranked,
                                                    selection.photo.get_attributes())]
-    print(photos_ranked)
-    photos = PhotoModel.objects.all()
     for i in range(len(photos_ranked)):
         if photos_ranked[i] > 0:
             photos_ranked[i] = 1
@@ -73,7 +71,7 @@ def selection_average_algorithm(user_model_instance, algorithm):
             photos_ranked[i] = -1
     best_list = []
     biggest_score = 0
-    for photo in photos:
+    for photo in PHOTOS:
         score = 0
         for x, y in zip(photo.get_attributes(), photos_ranked):
             if x == y:
@@ -97,10 +95,9 @@ def average_human_offset_algorithm(user_model_instance, algorithm):
         else:
             photos_ranked = [x - y for x, y in zip(photos_ranked,
                                                    selection.photo.get_attributes())]
-    photos = PhotoModel.objects.all()
     best_list = []
     biggest_score = -1000000
-    for photo in photos:
+    for photo in PHOTOS:
         score = sum(
             [x * y for x, y in zip(photos_ranked, photo.get_attributes())])
         if score > biggest_score:
@@ -129,10 +126,9 @@ def attribute_weights_on_test_sample_algorithm(user_model_instance, algorithm):
                 minusone[x][1] += 1
                 if isliked:
                     minusone[x][0] += 1
-    photos = PhotoModel.objects.all()
     best_list = []
     biggest_score = -1000000
-    for photo in photos:
+    for photo in PHOTOS:
         score = 0
         attributes = photo.get_attributes()
         for x in range(40):
@@ -167,10 +163,9 @@ def exponential_attribute_weights_on_test_sample_algorithm(user_model_instance,
                 minusone[x][1] += 1
                 if isliked:
                     minusone[x][0] += 1
-    photos = PhotoModel.objects.all()
     best_list = []
     biggest_score = -1000000
-    for photo in photos:
+    for photo in PHOTOS:
         score = 0
         attributes = photo.get_attributes()
         for x in range(40):
@@ -187,4 +182,3 @@ def exponential_attribute_weights_on_test_sample_algorithm(user_model_instance,
             best_list.append(photo)
     algorithm.photo = choice(best_list)
     algorithm.save()
-
